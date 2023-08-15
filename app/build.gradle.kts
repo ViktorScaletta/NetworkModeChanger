ec727f9b69bae0a6c943e249578c725aa8941b63
Viktor <viktorrr.garmashov@gmail.com>
2023-08-15 14:51:49 +0600

    - bump sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
     namespace = "com.gv.networkmodechanger"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.gv.networkmodechanger"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
